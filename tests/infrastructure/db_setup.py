71e2fc2065f569815a5dbd9eb41dd595298e100a
zrow <mrzrow@yandex.ru>
2025-03-29 02:47:14 +0300

    moved test session maker to db_setup

---
+++ tests/infrastructure/db_setup.py
@@ -0,0 +1,20 @@
+import pytest_asyncio
+
+from src.infrastructure.database.models.base import Base
+from src.infrastructure.database.session_maker import SessionMaker
+from tests.settings import test_settings
+
+test_session_maker = SessionMaker(
+    path=test_settings.db.path,
+    echo=test_settings.db.echo
+)
+
+
+@pytest_asyncio.fixture(scope='function', autouse=True)
+async def lifespan():
+    async with test_session_maker.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    try:
+        yield
+    finally:
+        await test_session_maker.engine.dispose()
--- tests/infrastructure/repositories/test_address.py
+++ tests/infrastructure/repositories/test_address.py
@@ -5,25 +5,8 @@ from datetime import datetime
 from decimal import Decimal
 
 from src.domain.entities.address import Address
-from src.infrastructure.database.models.base import Base
-from src.infrastructure.database.session_maker import SessionMaker
 from src.infrastructure.repositories.address import SQLAlchemyAddressRepository
-from tests.settings import test_settings
-
-test_session_maker = SessionMaker(
-    path=test_settings.db.path,
-    echo=test_settings.db.echo
-)
-
-
-@pytest_asyncio.fixture(scope='function', autouse=True)
-async def lifespan():
-    async with test_session_maker.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-    try:
-        yield
-    finally:
-        await test_session_maker.engine.dispose()
+from tests.infrastructure.db_setup import *
 
 
 @pytest.mark.asyncio
